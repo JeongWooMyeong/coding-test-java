4a3c75b5a8bd01f25d52fbf4b4db4b47f1790184
JeongWooMyeong <jwm9010@gmail.com>
2026-01-20 23:29:58 +0900

    코딩 테스트 자바

---
+++ src/그리디/큰수의법칙3_1.java
@@ -0,0 +1,46 @@
+package 그리디;
+
+import java.util.*;
+import java.io.*;
+
+public class 큰수의법칙3_1 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int answer = 0;
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] ia = new int[N];
+
+        for(int i=0;i<N;i++){
+            ia[i] = Integer.parseInt(st.nextToken());
+        }
+        //작은수 -> 큰수 정렬 오름차순 정렬
+        Arrays.sort(ia);
+
+        int data1 = ia[N-1]; //제일큰수
+        int data2 = ia[N-2]; // 두번째로 큰수
+        while(true) {
+            for (int j = 0; j < K; j++) {
+                if(M == 0) break;
+                answer += data1;
+                M--;
+            }
+
+            if(M==0) break;
+            answer += data2;
+            M--;
+
+
+        }
+
+        System.out.println(answer);
+
+    }
+}
---
+++ src/그리디/큰수의법칙3_2.java
@@ -0,0 +1,15 @@
+package 그리디;
+
+import java.util.*;
+import java.io.*;
+
+public class 큰수의법칙3_2 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //첫째줄 읽기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+    }
+
+}
