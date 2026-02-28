8e064b09bcf589a892d14ffedf37ba9dcae27575
JeongWooMyeong <jwm9010@gmail.com>
2026-02-28 16:38:40 +0900

    1.배웠던 알고리즘 복습 (DFS, BFS, 선택정렬, 삽입정렬, 퀵정렬, 계수정렬)
    2. 최단 경로
    (다익스트라, 플로이드워셜)

---
+++ src/이것이코딩테스트다2/다이나믹프로그래밍/효율적인화폐구성.java
@@ -0,0 +1,42 @@
+package 이것이코딩테스트다2.다이나믹프로그래밍;
+
+import java.util.*;
+
+public class 효율적인화폐구성 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //정수 N, M 입력 받기
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        //N개의 화폐 단위 정보를 입력 받기
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+        int[] d= new int[m+1];
+        Arrays.fill(d, 10001);
+
+        //다이나믹 프로그래밍 진행 (보텀업)
+        d[0] = 0;
+        for(int i=0;i<n;i++){
+            for(int j = arr[i];j<=m;j++){
+                //(i - k)원을 만드는 방법이 존재하는 경우
+                if(d[j - arr[i]] != 10001){
+                    d[j] = Math.min(d[j], d[j - arr[i] + 1]);
+                }
+            }
+        }
+
+        if(d[m] == 10001){
+            System.out.println(-1);
+        }else{
+            System.out.println(d[m]);
+        }
+
+    }
+
+}
---
+++ src/이것이코딩테스트다2/정렬/선택정렬4.java
@@ -0,0 +1,28 @@
+package 이것이코딩테스트다2.정렬;
+
+import java.util.*;
+
+public class 선택정렬4 {
+    public static void main(String[] args){
+        int n = 10;
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        for(int i=0;i<n;i++){
+            int min_index = i; //가장 작은 원소의 인덱스
+            for(int j=i+1;j<n;j++){
+                if(arr[min_index] > arr[j]){
+                    min_index = j;
+                }
+            }
+
+            //스와프
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
