cbe2a9ec85a41f05f323e0d4e38bf887b6bfc452
JeongWooMyeong <jwm9010@gmail.com>
2026-04-18 01:48:32 +0900

    260418
    여행경로 -1) 출발점이 ICN로 고정되어 있으므로 출발점을 ICN으로 잡고 해야함
    2) 그리고 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 함으로 Arrays.sort 해준다
    3) 백트래킹으로 visited 방문하고 다시 false로 변경해주는데 여기서 return이 배열인데 나는 list로 했으므로 두가지 방법으로 해봄
    -- String path 해서 list에 한번 담고 split (" ") 로 배열 return
    -- 애초부터 경로 path를 list로 담아서 진행
    visited 뿐만 아니라 path도 선택 → 탐색 → 선택을 취소(되돌림) 과정을 반복하는 겁니다. 이게 바로 백트래킹의 핵심이에요.

---
+++ src/프로그래머스/level3/여행경로.java
@@ -0,0 +1,52 @@
+package 프로그래머스.level3;
+
+
+import java.util.*;
+import java.io.*;
+
+public class 여행경로 {
+    static boolean[] visited;
+    static List<String> answer = new ArrayList<>();
+
+    public static String[] solution(String[][] tickets){
+        visited = new boolean[tickets.length];
+        //이렇게 정렬하는거 많이 하지 않아 익숙치 않음 -> 숙지 필요
+        Arrays.sort(tickets, (a,b)-> {
+           if(a[0].equals(b[0])) return a[1].compareTo(b[1]);
+           return a[0].compareTo(b[0]);
+        });
+        //시작점을 기준으로 하면되는구나
+        dfs("ICN", "ICN", tickets, 0);
+
+        return answer.get(0).split(" ");
+
+    }
+
+    static void dfs(String start, String path, String[][] tickets, int used){
+        if(used == tickets.length){
+            answer.add(path);
+            return;
+        }
+        //시작점이 2차원배열에서 첫번째랑 같은거 찾아서 다음 경로 찾음
+        for(int i=0;i<tickets.length;i++){
+            if(!visited[i] && tickets[i][0].equals(start)){
+                visited[i] = true;
+                dfs(tickets[i][1], path + " " + tickets[i][1], tickets, used+1);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        String[][] tickets = {{"ICN","JFK"},{"HND","IAD"},{"JFK","HND"}};
+        String[] arr = solution(tickets);
+
+        StringBuilder sb = new StringBuilder();
+        for(String str : arr){
+            sb.append(str).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
