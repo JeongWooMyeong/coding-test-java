a6b110f4a8cdc6f2bf0a6b8d61ade4d47662876b
JeongWooMyeong <jwm9010@gmail.com>
2026-05-28 20:37:54 +0900

    260528
    1. 표현가능한 이진트리
    
    - 포화 이진트리 - 2^h -1 개
    - 이진문자열 변경
    Long.toBinaryString(Long)
    - 그리고 포화 이진트리 개수 보다 작으면 h 증가
    - 그리고 가까운 포화 이진트리 개수 반환
    - 그리고 String format으로 길이만큼 앞에 0 추가
    - 가운데 root 기준으로 왼ㅉ고 오른쪽 재귀 dfs
    
    2. 길찾기 게임
    - y축 기준 내림차순
    - 처음이 root
    - root 기준으로 이진트리 insert
    - 전우 순우ㅟ 루트 왼 오
    - 후위 순위 왼 오 루트
    이렇게 출력

---
+++ src/프로그래머스/level3/길찾기게임6.java
@@ -0,0 +1,104 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 길찾기게임6 {
+    static ArrayList<Node> nodes;
+    static ArrayList<Integer> preorderList;
+    static ArrayList<Integer> postorderList;
+
+    static class Node implements Comparable<Node>{
+        int x,y, idx;
+        Node left, right;
+
+        public Node(int x, int y, int idx){
+            this.x = x;
+            this.y = y;
+            this.idx = idx;
+        }
+
+        public int compareTo(Node other){
+            if(this.y == other.y){
+                return this.x - other.x;
+            }
+
+            return other.y - this.y;
+        }
+
+    }
+
+    public static int[][] solution(int[][] nodeinfo){
+
+        nodes = new ArrayList<>();
+        preorderList = new ArrayList<>();
+        postorderList = new ArrayList<>();
+
+        int idx = 1;
+        for(int[] node : nodeinfo){
+            int x = node[0];
+            int y = node[1];
+
+            nodes.add(new Node(x,y,idx));
+
+            idx++;
+
+        }
+
+        Collections.sort(nodes);
+
+        Node root = nodes.get(0);
+        for(int i=1;i<nodes.size();i++){
+            insertNodes(root, nodes.get(i));
+        }
+
+        preOrder(root);
+        postOrder(root);
+
+        int[][] answer = new int[2][preorderList.size()];
+
+        for(int i=0;i<preorderList.size();i++){
+            answer[0][i] = preorderList.get(i);
+            answer[1][i] = postorderList.get(i);
+        }
+
+        return answer;
+
+    }
+
+    static void insertNodes(Node parent, Node child){
+        if(parent.x > child.x){
+            if(parent.left == null) {
+                parent.left = child;
+            }else{
+                insertNodes(parent.left, child);
+            }
+        }else{
+            if(parent.right == null){
+                parent.right = child;
+            }else{
+                insertNodes(parent.right, child);
+            }
+        }
+    }
+
+    static void preOrder(Node root){
+        if(root == null) return;
+        preorderList.add(root.idx);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    static void postOrder(Node root){
+        if(root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        postorderList.add(root.idx);
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[][] nodeinfi = {{5,3},{11,5},{13,3},{3,5},{6,1},{1,3},{8,6},{7,2},{2,2}};
+        System.out.println(Arrays.deepToString(solution(nodeinfi)));
+    }
+
+}
