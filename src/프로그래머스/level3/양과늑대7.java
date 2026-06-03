795722aee999394a671808443c623e567120b3fd
JeongWooMyeong <jwm9010@gmail.com>
2026-06-03 15:51:38 +0900

    260603
    1. 디펜스게임
    - 우선순위 큐 사용하여
    - sum 누적하면서 n 넘기면
    가장 큰 숫자 빼면서 k 무적권 사용 (최적)
    
    2. 양과늑대
    - child 다 dfs 돌려서 해야함
    List<Integer>
    
    3. 다단계칫솔판매
    - Map parent Map profit 이용하여 구하기

---
+++ src/프로그래머스/level3/양과늑대7.java
@@ -0,0 +1,68 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 양과늑대7 {
+
+    static ArrayList<ArrayList<Integer>> graph;
+    static int n;
+    static int answer;
+
+    public static int solution(int[] info, int[][] edges){
+        answer = Integer.MIN_VALUE;
+        n = info.length;
+        graph = new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] e : edges){
+            int parent = e[0];
+            int child = e[1];
+
+            graph.get(parent).add(child);
+        }
+
+        List<Integer> nextnodes = new ArrayList<>();
+        nextnodes.add(0);
+
+        dfs(0,0,nextnodes,info);
+
+        return answer;
+
+    }
+
+    static void dfs(int sheep, int wolf, List<Integer> nextnodes, int[] info){
+        answer = Math.max(answer, sheep);
+
+        for(int current : nextnodes){
+            int ns = sheep;
+            int nw = wolf;
+
+            if(info[current] == 0){
+                ns++;
+            }else{
+                nw++;
+            }
+
+            if(nw >= ns) continue;
+
+            List<Integer> candidates = new ArrayList<>(nextnodes);
+            candidates.remove(Integer.valueOf(current));
+            candidates.addAll(graph.get(current));
+
+            dfs(ns,nw,candidates,info);
+
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[] info = {0,1,0,1,1,0,1,0,0,1,0};
+        int[][] edges = {{0,1},{0,2},{1,3},{1,4},{2,5},{2,6},{3,7},{4,8},{6,9},{9,10}};
+
+        System.out.println(solution(info, edges));
+    }
+
+}
