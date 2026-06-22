cb6c7221475275eec1a0ad997849425426f31a0c
JeongWooMyeong <jwm9010@gmail.com>
2026-06-22 20:29:05 +0900

    260622
    트리
    
    1. 양과늑대
    - 트리 dfs
    - graph -> 부모, 자식 리스트
    - root 관련 nextNodes 구하고 dfs 돌림
    - root일때 양인지 늑대인지 확인
    - 늑대 >= 양 continue;
    - 아니라면 자식노드들 구해서 복사 (현재 노드 삭제)
    - 나머지 자식노드들 담고 dfs 진행
    
    2. 길찾기 게임
    - 구조체 좌표 이용하여 트리 생성 전위 순회, 후위 순회 구하는거
    - y축 내림차순, x축 오름차순 정렬
    - 정려 후 처음이 루트 노드
    - 전위 순회 - 루 왼 오
    - 후위 순회 - 왼 오 루
    
    3. 다단계 칫솔판매
    - Map paretn Map profit ㅎ서
    - 리프 노드로부터 루트 노드까지 profit 누적 구한다음 return
    
    4. 표현가능한 이진트리
    - 해당 숫자 이진트리 string 표현
    - 그리고 트리는 2^K -1 개수에 맞아야 하므로
    - 맞추고 앞에 가까운 개수만큼 0 채우기
    - 그리고 검증
    - mid - root (length / 2)
    - mid 기준으로 left , right string 나눔
    - 안될 경우 root = '0' 일땐 서브트리에 1들어가면 fasle
    - 그리고 리프까지 검증
    - true면 1 , 아니면 0
    
    5. 매출하락최소화
    - dp[node][0] 해당 노드 참석 X
    - dp[node][1] 해당 노드 참석 O
    - 돌리면서 리프노드는 간선 비어 있으므로 처리 해줘야 값 쓰레기 발생 안함
    - child 돌면서
    - 참석하는 경우 참석 안할때보다 작으면 ture
    - 모두 참석 안할때 한명이라도 참석해야하므로 extra (참석하는 경우 참석하지 않는경우 차이 - 참석 비용)
    
    -리프노드로부터 올라가야하므로 dfs(Child, sales) 우선 진행
    - 루트 dp[1][0] dp[1]1[] 최소값 리턴
    
    6. 동굴탐험
    - before, after, visited, 정점간선
    - before 선행작업
    - 큐에서 꺼낼때 after 있으면 꺼내서 방문처리 후 넣기
    - next 넣을때 방문처리 안되어 ㅇㅆ고 선행작업ㅇ 있으면 열리는 after 넣고 continue
    - 이렇게 하고 visited가 다 방문처리 되어 잇으면 true
    - 하나라도 false 이면 false truen

---
+++ src/프로그래머스/level3/양과늑대11.java
@@ -0,0 +1,74 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 양과늑대11 {
+
+    static ArrayList<ArrayList<Integer>> graph;
+    static int n;
+    static int answer;
+
+    public static int solution(int[] info, int[][] edges){
+        answer = 0;
+        n = info.length;
+
+        graph = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] e : edges){
+            int parent = e[0];
+            int child = e[1];
+
+            graph.get(parent).add(child);
+        }
+
+        int root = 0;
+        int sheep = info[root] == 0 ? 1 : 0;
+        int wolf = info[root] == 1 ? 1 : 0;
+
+        List<Integer> nextNodes = new ArrayList<>(graph.get(root));
+
+
+        dfs(sheep,wolf,nextNodes,info);
+
+        return answer;
+
+    }
+
+    static void dfs(int sheep, int wolf, List<Integer> nextNodes, int[] info){
+        answer = Math.max(answer, sheep);
+
+        for(int current : nextNodes){
+            int ns = sheep;
+            int nw = wolf;
+
+            if(info[current] == 0){
+                ns++;
+            }else{
+                nw++;
+            }
+
+            if(nw >= ns) continue;
+
+            List<Integer> candidates = new ArrayList<>(nextNodes);
+            candidates.remove(Integer.valueOf(current));
+
+            candidates.addAll(graph.get(current));
+
+            dfs(ns, nw, candidates, info);
+
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[] info = {0,1,0,1,1,0,1,0,0,1,0};
+        int[][] edges = {{0,1},{0,2},{1,3},{1,4},{2,5},{2,6},{3,7},{4,8},{6,9},{9,10}};
+
+        System.out.println(solution(info, edges));
+    }
+
+}
