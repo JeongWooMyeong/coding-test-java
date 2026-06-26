4a06b00a7c4b468b07d34d43eb73d67d12fadb26
JeongWooMyeong <jwm9010@gmail.com>
2026-06-26 22:18:39 +0900

    260626
    1. 네트워크
    - find-union
    - bfs or dfs로 풀 수 있음
    
    2. 지형이동
    - bfs + MST 응용 문제
    - hieght 이하 bfs
    - group번호기준 간선 다고
    - 크루스칼 or 프림 알고리즘
    
    3. 섬연결하기
    - MST
    - 프림 알고리즘으로 풀음
    
    3.

---
+++ src/프로그래머스/level3/섬연결하기28.java
@@ -0,0 +1,85 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+/*
+프림 알고리즘
+ */
+
+public class 섬연결하기28 {
+
+    static ArrayList<ArrayList<Edge>> edges;
+    static int[] d;
+    static int INF = (int) 1e9;
+    static boolean[] visited;
+    static class Edge implements Comparable<Edge>{
+        int to;
+        int cost;
+
+        public Edge(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+
+        public int compareTo(Edge other){
+            return this.cost - other.cost;
+        }
+
+    }
+
+    public static int solution(int n, int[][] costs){
+        int answer = 0;
+
+        edges = new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            edges.add(new ArrayList<>());
+        }
+
+        for(int[] c : costs){
+            int a = c[0];
+            int b = c[1];
+            int cost = c[2];
+
+            edges.get(a).add(new Edge(b,cost));
+            edges.get(b).add(new Edge(a,cost));
+        }
+
+        d = new int[n];
+        Arrays.fill(d, INF);
+        visited = new boolean[n];
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(0, 0));
+        d[0] = 0;
+
+        while(!pq.isEmpty()){
+            Edge cur = pq.poll();
+            int now = cur.to;
+            int dist = cur.cost;
+            if(visited[now]) continue;
+            visited[now] = true;
+            answer += dist;
+            for(int i=0;i<edges.get(now).size();i++){
+                int next = edges.get(now).get(i).to;
+                int cost= edges.get(now).get(i).cost;
+                if(d[next] > cost){
+                    d[next] = cost;
+                    pq.offer(new Edge(next,cost));
+                }
+            }
+        }
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception{
+        int n = 4;
+        int[][] costs = {{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}};
+
+        System.out.println(solution(n, costs));
+    }
+
+}
