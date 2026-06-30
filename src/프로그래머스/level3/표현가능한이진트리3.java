8e3b011008c602bf9930105b3e11e483ecf45ddc
JeongWooMyeong <jwm9010@gmail.com>
2026-06-30 22:06:51 +0900

    260630
    1. 양과늑대
    그래프 만들기
    
    주어진 edges로 트리를 구성합니다. (부모 → 자식)
    
    DFS 탐색
    
    현재까지 모은 양(sheep)과 늑대(wolf) 수를 관리합니다.
    
    후보 노드(nextNodes)를 리스트로 두고, 현재 방문할 수 있는 모든 노드를 저장합니다.
    
    조건 체크
    
    어떤 노드를 방문했을 때 늑대 수가 양 수 이상이 되면 탐색 중단.
    
    그렇지 않으면 계속 탐색.
    
    후보 갱신
    
    현재 노드를 방문하면 후보 리스트에서 제거하고, 그 노드의 자식들을 후보에 추가합니다.
    
    이렇게 해서 "갈 수 있는 모든 경로"를 탐색합니다.
    
    정답 갱신
    
    탐색 중에 얻을 수 있는 최대 양의 수를 answer에 기록합니다.
    
    2. 길찾기 게임
    
    - 좌표 - 이진트리 구성
    - y축 내림차순, x 오름차순
    - 정렬 후 처음이 root
    - root 노드 기준으로 x좌표 이용하여 node left, right 에 넣기
    - 전위 순회, 후위 순회 진행
    - 전위 순회 - 루 왼 오
    - 후위 순회 - 왼 오 루
    - 리스트에 담고
    - 배열로 받아서 return
    
    3. 표현 가능한 이진 트리
    - Long.toBinaryString 으로 이진수 변형
    - 포화트리 개수 2 * size + 1
    - binary.length() 미만만큼 size 구하기
    - 앞에 0채우고 뒤에 binary 채우고
    - check(binary)
    - mid (중간) - root
    - left2 ,right2 나눠서 하위까지 진행
    - 만약 root 가 0 이면
    밑에 하위 노드에 1이 하나라도 있으면 false
    - 길이 1라면 무조건 true
    - 하위까지 무조건 true이면 true
    
    4. 다단계칫솔판매
    - parent, profit
    - parent, profit 0 초기화
    - money = amount * 100
    - need = money / 10
    - money > 0 될때까지
    - keep = moeny - need;
    - keep current profit 저장
    - 위의 root로 올라가서 계속 진행
    - answer 배열에 profit map 데이터 받아오고 return

---
+++ src/프로그래머스/level3/표현가능한이진트리3.java
@@ -0,0 +1,61 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 표현가능한이진트리3 {
+
+    static int[] answer;
+
+    public static int[] solution(long[] numbers){
+
+        answer = new int[numbers.length];
+
+        for(int i=0;i<numbers.length;i++){
+            String binary = Long.toBinaryString(numbers[i]);
+            int size = 1;
+            while(size < binary.length()){
+                size = 2 * size + 1;
+            }
+
+            binary = "0".repeat(size - binary.length()) + binary;
+
+
+            if(check(binary)){
+                answer[i] = 1;
+            }else{
+                answer[i] = 0;
+            }
+
+        }
+
+        return answer;
+
+    }
+
+    static boolean check(String binary){
+
+        if(binary.length() == 1) return true;
+
+        int left = 0;
+        int right = binary.length();
+
+        int mid = (left + right) / 2;
+        String left2 = binary.substring(0, mid);
+        String right2 = binary.substring(mid+1);
+
+        if(binary.charAt(mid) == '0'){
+            if(left2.contains("1") || right2.contains("1")){
+                return false;
+            }
+        }
+
+        return check(left2) && check(right2);
+    }
+
+    public static void main(String[] args) throws Exception{
+        long[] numbers = {7, 42, 5};
+        System.out.println(Arrays.toString(solution(numbers)));
+    }
+
+}
