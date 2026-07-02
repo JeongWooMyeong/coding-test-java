2a27bbb77aff9036c3529c151e2140bde119fd08
JeongWooMyeong <jwm9010@gmail.com>
2026-07-02 21:17:27 +0900

    260702
    DP (반복해서 써야하는 경우 최대 최소 경우의 수) 어렵다 이게
    
    1. 코딩테스트공부
    - maxalp, maxcop 가 범위
    - 다익스트라 변형
    - dp 배열 INF 초기화
    - alp, cop min
    - 우선순위 큐
    - 알고력 올리기, 코딩력 올리기
    - 문제 풀기
    - 3가지 경우 구해서 큐에 넣기
    
    2. 숫자타자대회
    - 0~9 좌표 저장 (2차원 배열)
    - dp[left][right][idx] - left, right, idx일때 최소비용
    - 왼손으로 누를 경우 오른손으로 누를 경우 최소 비용 구하기
    - 카운트는 daegak 맨해튼 거리 최소
    - 직선거리 최대 - 대각
    
    3. 최적의행렬곱셈
    - dp[i][j] = i번째 행렬부터 j번째 행렬까지 곱했을 때 필요한 최소 연산 횟수
    - len i j 따라서 cost 구하고 더 작으면 갱신
    - dp[0][n-1]
    
    4. 연속펄스부분수열의합
    - 카데인 알고리즘 활용
    - dp1[i] = 펄스 패턴 1(짝수 +, 홀수 -)에서 i번째 원소까지 고려했을 때, i번째 원소를 포함하는 최대 부분합
    
      dp2[i] = 펄스 패턴 2(짝수 -, 홀수 +)에서 i번째 원소까지 고려했을 때, i번째 원소를 포함하는 최대 부분합

---
+++ src/프로그래머스/level3/숫자타자대회7.java
@@ -0,0 +1,82 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 숫자타자대회7 {
+
+    static int[][][] dp;
+    static int[] numarr;
+    static int[][] pos = {
+            {3,1},
+            {0,0},
+            {0,1},
+            {0,2},
+            {1,0},
+            {1,1},
+            {1,2},
+            {2,0},
+            {2,1},
+            {2,2}
+    };
+
+    public static int solution(String numbers){
+        numarr = new int[numbers.length()];
+        for(int i=0;i<numbers.length();i++){
+            numarr[i] = numbers.charAt(i) - '0';
+        }
+
+        dp = new int[10][10][numarr.length];
+
+        for(int[][] d1 : dp){
+            for(int[] d2 : d1){
+                Arrays.fill(d2, -1);
+            }
+        }
+
+        int answer = 0;
+        answer = dfs(4,6,0);
+
+        return answer;
+    }
+
+    static int dfs(int left, int right, int idx){
+        if(idx == numarr.length) return 0;
+        if(dp[left][right][idx] != -1) return dp[left][right][idx];
+
+        int target = numarr[idx];
+        int leftCount = Integer.MAX_VALUE;
+        int rightCount = Integer.MAX_VALUE;
+
+        if(target != right){
+            leftCount = getCount(left, target) + dfs(target,right,idx+1);
+        }
+
+        if(target != left){
+            rightCount = getCount(right, target) + dfs(left, target,idx+1);
+        }
+
+        return dp[left][right][idx] = Math.min(leftCount, rightCount);
+    }
+
+    static int getCount(int a, int b){
+        if(a==b) return 1;
+        int[] a1 = pos[a];
+        int[] b1 = pos[b];
+
+        int dx = Math.abs(a1[0] - b1[0]);
+        int dy = Math.abs(a1[1] - b1[1]);
+
+        int daegak = Math.min(dx, dy);
+        int straight = Math.max(dx,dy) - daegak;
+
+        return 3 * daegak + 2 * straight;
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        String numbers = "1756";
+        System.out.println(solution(numbers));
+    }
+
+}
