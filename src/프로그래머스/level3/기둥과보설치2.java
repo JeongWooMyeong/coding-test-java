2dd91089bff1bdaed97cee92e179fde979e69f37
JeongWooMyeong <jwm9010@gmail.com>
2026-07-03 22:42:52 +0900

    260703
    구현
    
    1. 기둥과 보설치
    - List<int[]> result;
    - 설치할때 해보고 check (모든 result에 대해서 조건 만족 확인
    - 삭제할때도 해보고 만족 못하면 다시 넣기
    - 그래서 result 오름차순 정렬하여 answer 배열 return
    
    2. 자물쇠와 열쇠
    - lock 배열 3배확장
    - 가운데에 lock 배열 넣고
    - key rotate 하면서 match 확인 후 일치하면 true
    - 매칭되는거 없으면 false return

---
+++ src/프로그래머스/level3/기둥과보설치2.java
@@ -0,0 +1,91 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 기둥과보설치2 {
+
+    static List<int[]> result;
+
+    public static int[][] solution(int n, int[][] build_frame){
+        result = new ArrayList<>();
+
+        for(int[] build : build_frame){
+            int x = build[0];
+            int y = build[1];
+            int type = build[2];
+            int cmd = build[3];
+            //설치
+            if(cmd == 1){
+                result.add(new int[]{x,y,type});
+                if(!check()){
+                    result.remove(result.size()-1);
+                }
+            }
+            //삭제
+            else{
+                int[] target = new int[]{x,y,type};
+                result.removeIf(arr -> Arrays.equals(arr, target));
+                if(!check()){
+                    result.add(target);
+                }
+
+
+            }
+
+        }
+
+        result.sort((a,b)->{
+           if(a[0]!=b[0]) return a[0] - b[0];
+           if(a[1]!=b[1]) return a[1] - b[1];
+           return a[2] - b[2];
+        });
+
+        int[][] answer = new int[result.size()][3];
+        for(int i=0;i<result.size();i++){
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+
+    static boolean check(){
+        for(int[] arr : result){
+            int x = arr[0];
+            int y = arr[1];
+            int type = arr[2];
+            //기둥일때
+            if(type == 0){
+                if(y ==0 || contains(x,y-1,0) || contains(x-1,y,1) || contains(x,y,1)){
+                    continue;
+                }
+                return false;
+            }else{
+                if(contains(x,y-1,0) || contains(x+1,y-1,0) || (contains(x-1,y,1) && contains(x+1,y,1))){
+                    continue;
+                }
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    static boolean contains(int x, int y, int type){
+        for(int[] arr : result){
+            if(arr[0] == x && arr[1] == y && arr[2] == type) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception{
+        int n = 5;
+        int[][] build_frame = 	{{1,0,0,1},{1,1,1,1},{2,1,0,1},{2,2,1,1},{5,0,0,1},{5,1,0,1},{4,2,1,1},{3,2,1,1}};
+
+        System.out.println(Arrays.deepToString(solution(n, build_frame)));
+    }
+
+
+
+}
---
+++ src/프로그래머스/level3/자물쇠와열쇠6.java
@@ -0,0 +1,77 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 자물쇠와열쇠6 {
+    static int[][] board;
+    static int n,m;
+
+    public static boolean solution(int[][] key, int[][] lock){
+        n = lock.length;
+        m = key.length;
+
+        board = new int[3*n][3*n];
+
+        for(int i=n;i<2*n;i++){
+            for(int j=n;j<2*n;j++){
+                board[i][j] = lock[i%n][j%n];
+            }
+        }
+
+        for(int x=0;x<2*n;x++){
+            for(int y=0;y<2*n;y++){
+                for(int d=0;d<4;d++){
+                    for(int i=0;i<m;i++){
+                        for(int j=0;j<m;j++){
+                            board[x+i][y+j] += key[i][j];
+                        }
+                    }
+
+                    if(match()) return true;
+
+                    for(int i=0;i<m;i++){
+                        for(int j=0;j<m;j++){
+                            board[x+i][y+j] -= key[i][j];
+                        }
+                    }
+
+                    key = rotate(key);
+
+                }
+            }
+        }
+
+        return false;
+    }
+
+    static int[][] rotate(int[][] key){
+        int[][] result = new int[m][m];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                result[i][j] = key[j][m-1-i];
+            }
+        }
+
+        return result;
+    }
+
+    static boolean match(){
+        for(int i=n;i<2*n;i++){
+            for(int j=n;j<2*n;j++){
+                if(board[i][j] != 1) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[][] key ={{0,0,0},{1,0,0},{0,1,1}};
+        int[][] lock = {{1,1,1},{1,1,0},{1,0,1}};
+
+        System.out.println(solution(key, lock));
+    }
+
+}
