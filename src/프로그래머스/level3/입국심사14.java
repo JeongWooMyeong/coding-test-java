6f37004e5555fd4b8b9780c96323e2340b2f5e82
JeongWooMyeong <jwm9010@gmail.com>
2026-07-03 15:23:27 +0900

    260703
    1. 가사검색
    - lowerbound, upperbound 활용
    - 길이별 map, reversemap List<String> 담기
    - List<String> 정렬
    
    2. 입국심사
    - 파라메트릭 서치
    - 심사하는데 걸리는 시간 mid
    - count += mid / times;
    - count >= n 이면 mid 줄여봄
    - 갱신된 answer값 return

---
+++ src/프로그래머스/level3/입국심사14.java
@@ -0,0 +1,42 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 입국심사14 {
+
+    public static long solution(int n, int[] times){
+
+        Arrays.sort(times);
+
+        long left = 0;
+        long right = (long)times[times.length-1] * n;
+        long answer = 0;
+
+        while(left <= right){
+            long mid = (left + right) / 2;
+            long count = 0;
+            for(int x : times){
+                count += mid / x;
+            }
+
+            if(count >= n){
+                answer = mid;
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception{
+        int n = 6;
+        int[] times = {7,10};
+
+        System.out.println(solution(n, times));
+    }
+
+}
