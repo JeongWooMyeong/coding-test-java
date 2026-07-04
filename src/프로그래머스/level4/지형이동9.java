25be794e34615da900df65a18377b2ef1992510d
JeongWooMyeong <jwm9010@gmail.com>
2026-07-04 18:03:35 +0900

    260704
    1. 외벽점검
    - 배열 2배 확장
    - 친구 순열
    - weak 시작점 기준 친구 수 확인
    - 친구수 안나올 경우도 확인
    - return 친구수
    
    2. 동굴탐험
    - bfs
    - 선행 방문 int[] before
    - 방문 후 들어갈 수있는 방 after
    - 배열 -1 초기화
    - bfs 시행
    -> 큐에 담고 뺄때 after 여린방 방문처리 및 큐에 담음
    -> next들 방문 확인, before 있으면 after 처리
    -> 둘다 아니라면 방문처리 및 큐에 담음
    - visited가 다 방문되지 않았다면 false 다 방문했다면 true
    
    3. 지형이동
    - bfs + MST ( 크루스칼 or 프림 알고리즘)
    - bfs로 height 이하 그룹화
    - 그룹된 배열 이용하여 간선 담고
    MST 알고리즘 시행
    
    4. 코딩테스트공부
    - 다익스트라 변형
    - dp[a][c] -> 알고력 a 코딩력 c올리는데 걸리는 최소시간
    - 코딩력 올리기, 알고력 올리기, 문제 풀기 세가지 우선순위 큐 이용하여 최소비용 구한다.

---
+++ src/프로그래머스/level4/지형이동9.java
@@ -0,0 +1,145 @@
+package 프로그래머스.level4;
+
+import java.util.*;
+import java.io.*;
+
+public class 지형이동9 {
+
+    static ArrayList<Edge> edges;
+    static int[] parent;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
+    static int[][] map;
+    static int n,m;
+    static boolean[][] visited;
+
+    static class Edge implements Comparable<Edge>{
+        int from, to, cost;
+        public Edge(int from, int to, int cost){
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+
+        public int compareTo(Edge other){
+            return this.cost - other.cost;
+        }
+    }
+
+    public static int solution(int[][] land, int height){
+
+        int answer = 0;
+        edges = new ArrayList<>();
+        n = land.length;
+        m = land[0].length;
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        int groupid = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!visited[i][j]){
+                    bfs(i,j,groupid, land,height);
+                    groupid++;
+                }
+            }
+        }
+
+        parent = new int[groupid];
+        for(int i=0;i<groupid;i++){
+            parent[i] = i;
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int d=0;d<4;d++){
+                    int ni = i + dx[d];
+                    int nj = j + dy[d];
+
+                    if(ni < 0 || nj < 0 || ni >= n || nj >= m) continue;
+
+                    int a = map[i][j];
+                    int b = map[ni][nj];
+
+                    if(a == b) continue;
+
+                    int diff = Math.abs(land[i][j] - land[ni][nj]);
+
+                    edges.add(new Edge(a,b,diff));
+
+                }
+            }
+        }
+
+        Collections.sort(edges);
+
+        for(int i=0;i<edges.size();i++){
+            int a = edges.get(i).from;
+            int b = edges.get(i).to;
+            int cost = edges.get(i).cost;
+
+            if(findParent(a) != findParent(b)){
+                union(a,b);
+                answer += cost;
+            }
+
+        }
+
+        return answer;
+    }
+
+    static void bfs(int x, int y, int groupid, int[][] land, int height){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x,y});
+        visited[x][y] = true;
+        map[x][y] = groupid;
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x1 = cur[0];
+            int y1 = cur[1];
+
+            for(int i=0;i<4;i++){
+                int nx = x1 + dx[i];
+                int ny = y1 + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+
+                if(visited[nx][ny]) continue;
+
+                int diff = Math.abs(land[x1][y1] - land[nx][ny]);
+
+                if(diff <= height){
+                    visited[nx][ny] = true;
+                    map[nx][ny] = groupid;
+                    q.offer(new int[]{nx,ny});
+                }
+
+
+            }
+
+        }
+    }
+
+    static int findParent(int x){
+        if(parent[x] == x) return x;
+        else return parent[x] = findParent(parent[x]);
+    }
+
+    static void union(int x, int y){
+        int a = findParent(x);
+        int b = findParent(y);
+
+        if(b > a) parent[b] = a;
+        else parent[a] = b;
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[][] land = {{1,4,8,10},{5,5,5,5},{10,10,10,10},{10,10,10,20}};
+        int height = 3;
+
+        System.out.println(solution(land, height));
+    }
+
+}
