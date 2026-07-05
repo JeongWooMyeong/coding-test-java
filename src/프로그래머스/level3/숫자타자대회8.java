07719757ca0232fcbb2025c460eee2b6074f42fa
JeongWooMyeong <jwm9010@gmail.com>
2026-07-05 19:29:41 +0900

    260705
    1. 퍼즐조각 채우기
    - 빈칸 채우기 (dfs)
    - 블록 채우기 (dfs)
    - 빈칸과 블록 매칭
    - 매칭도니ㅡㄴ거 answer 에 누적
    - return answer;
    
    2. 블록이동하기
    - bfs 최단경로
    - 드론 2가지 경우
    - 4가지 방향, 드론 회전중 최단으로 도착점에 도착하는 경우 return
    
    3. 숫자타자대회
    - 숫자 2차원 배열 좌표 저장
    - DP[left][right][idx] - left, right 위치에서 idx 누를 경우 최소비용
    - 왼쪽에서 누를경우, 오른쪽에서 누를 경우 최소 비용 구하기
    - 메모이제이션 활용
    
    4. 최적의행렬곱셈
    - dp[i][j] - i~j범위까지 행렬 연산 최소 횟수
    - len 길이만큼 i,k,j 범위 나눠서 연산횟수 최소 구함
    - dp[0][n-1] return
    
    5. 자물쇠와 열쇠
    - board 3배 확장 2차원 배열
    - 가운데에 lock 넣기
    - key 이동하면서 끼우면서 lock 모두 1 만족하는 지 확인
    - 만족하면 true 다 돌았는데 아니면 false
    - 회전 가능 (key)
    
    6.기둥과 보설치
    - List<int[]> result (최종적으ㅗ 보와 기둥 설치 list
    - 설치 일때 삭제일때 넣어보면서 모든 보와 기둥이 만족하는지 확인
    - 아니라면 설치일때는 삭제 삭제일때는 설치 진행 (복구)
    - 담은 result 정렬 진행하고
    - 2차원 answer에 담아서 return
    - 기둥 바닥위 (y==0), 다른 기둥위 (x,y-1), 보의 한족 끝부분(x-1,y) , (x,y)
    - 보 한쪽 끝 부분이 기둥위에 있음
    (x,y-1), (x+1, y-1)
    양쪽 끝 부분이 다른 보와 동시에 연결
    (x-1,y) && (x+1,y)

---
+++ src/프로그래머스/level3/숫자타자대회8.java
@@ -0,0 +1,85 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 숫자타자대회8 {
+
+    static int[] numarr;
+    static int[][][] dp;
+    static int[][] pos = {
+            {3,1},
+            {0,0},
+            {0,1},
+            {0,2},
+            {1,0},
+            {1,1},
+            {1,2},
+            {2,0},
+            {2,1},
+            {2,2},
+    };
+
+    public static int solution(String numbers){
+        numarr = new int[numbers.length()];
+
+        for(int i=0;i<numbers.length();i++){
+            numarr[i] = numbers.charAt(i) - '0';
+        }
+
+        dp = new int[10][10][numarr.length];
+
+        for(int[][] d1 : dp){
+           for(int[] d2 : d1){
+               Arrays.fill(d2, -1);
+           }
+        }
+
+        int answer = 0;
+        answer = dfs(4,6,0);
+
+        return answer;
+
+    }
+
+    static int dfs(int left, int right, int idx){
+        if(idx == numarr.length) return 0;
+        if(dp[left][right][idx] != -1) return dp[left][right][idx];
+
+        int leftCount = Integer.MAX_VALUE;
+        int rightCount = Integer.MAX_VALUE;
+        int target = numarr[idx];
+
+        if(right != target){
+            leftCount = getCount(left, target) + dfs(target,right,idx+1);
+        }
+
+        if(left != target){
+            rightCount = getCount(right, target) + dfs(left, target, idx+1);
+        }
+
+        return dp[left][right][idx] = Math.min(leftCount, rightCount);
+
+    }
+
+    static int getCount(int a, int b){
+        if(a == b) return 1;
+        int[] a1 = pos[a];
+        int[] a2 = pos[b];
+
+        int dx = Math.abs(a1[0]-a2[0]);
+        int dy = Math.abs(a1[1]-a2[1]);
+
+        int daegak = Math.min(dx,dy);
+        int straight = Math.max(dx,dy) - daegak;
+
+        return 3 * daegak + 2 * straight;
+    }
+
+    public static void main(String[] args) throws Exception{
+        String numbers = "1756";
+        System.out.println(solution(numbers));
+    }
+
+
+}
---
+++ src/프로그래머스/level3/퍼즐조각채우기16.java
@@ -0,0 +1,145 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 퍼즐조각채우기16 {
+
+    static boolean[][] visited;
+    static int n,m;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
+
+    public static int solution(int[][] game_board, int[][] table){
+        int answer = 0;
+        n = game_board.length;
+        m = game_board[0].length;
+
+        List<List<int[]>> blanks = new ArrayList<>();
+        visited = new boolean[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!visited[i][j] && game_board[i][j] == 0){
+                    List<int[]> shape = new ArrayList<>();
+                    dfs(i,j,shape,game_board,0);
+                    blanks.add(normalize(shape));
+                }
+            }
+        }
+
+        List<List<int[]>> blocks = new ArrayList<>();
+        visited = new boolean[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!visited[i][j] && table[i][j] == 1){
+                    List<int[]> shape = new ArrayList<>();
+                    dfs(i,j,shape,table,1);
+                    blocks.add(normalize(shape));
+                }
+            }
+        }
+
+        boolean[] used = new boolean[blocks.size()];
+
+        for(List<int[]> blank : blanks){
+            for(int i=0;i<blocks.size();i++){
+                if(blank.size() != blocks.get(i).size()) continue;
+                if(used[i]) continue;
+
+                List<int[]> rotated = blocks.get(i);
+
+                for(int d=0;d<4;d++){
+                    if(match(rotated,blank)){
+                        used[i] = true;
+                        answer += blank.size();
+                        break;
+                    }
+
+                    rotated = rotate(rotated);
+
+                }
+
+                if(used[i]) break;
+
+            }
+        }
+
+        return answer;
+
+    }
+
+    static void dfs(int x, int y , List<int[]> shape, int[][] board, int target){
+        visited[x][y] = true;
+        shape.add(new int[]{x,y});
+
+        for(int i=0;i<4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if(visited[nx][ny]) continue;
+
+            if(board[nx][ny] == target){
+                dfs(nx,ny,shape,board,target);
+            }
+
+        }
+
+    }
+
+    static List<int[]> normalize(List<int[]> shape){
+        List<int[]> result = new ArrayList<>();
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+
+        for(int[] s : shape){
+            minX = Math.min(minX, s[0]);
+            minY = Math.min(minY, s[1]);
+        }
+
+        for(int[] s : shape){
+            result.add(new int[]{s[0] - minX, s[1] - minY});
+        }
+
+        return result;
+    }
+
+    static List<int[]> rotate(List<int[]> shape){
+        List<int[]> result = new ArrayList<>();
+        int maxX = Integer.MIN_VALUE;
+        for(int[] s : shape){
+            maxX = Math.max(maxX, s[0]);
+        }
+
+        for(int[] s : shape) {
+            result.add(new int[]{s[1], maxX - s[0]});
+        }
+
+        return normalize(result);
+    }
+
+    static boolean match(List<int[]> p1, List<int[]> p2){
+        if(p1.size() != p2.size()) return false;
+        p1.sort((a,b)-> (a[0] == b[0]) ? a[1] - b[1] : a[0] - b[0]);
+        p2.sort((a,b)-> (a[0] == b[0]) ? a[1] - b[1] : a[0] - b[0]);
+
+        for(int i=0;i<p1.size();i++){
+            if(p1.get(i)[0] != p2.get(i)[0] || p1.get(i)[1] != p2.get(i)[1]) return false;
+        }
+
+
+        return true;
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[][] game_board = {{1,1,0,0,1,0},{0,0,1,0,1,0},{0,1,1,0,0,1},{1,1,0,1,1,1},{1,0,0,0,1,0},{0,1,1,1,0,0}};
+        int[][] table =  {{1,0,0,1,1,0},{1,0,1,0,1,0},{0,1,1,0,1,1},{0,0,1,0,0,0},{1,1,0,1,1,0},{0,1,0,0,0,0}};
+
+        System.out.println(solution(game_board, table));
+    }
+
+
+}
