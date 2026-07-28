3f4faed8710d711beb2bea1e12791962084942fd
JeongWooMyeong <jwm9010@gmail.com>
2026-07-28 19:01:34 +0900

    260728
    1. 퍼즐조각 채우기
    - 빈칸 채우기 (dfs)
    - 블록 채우기 (dfs)
    - 빈칸 ,블록 매칭 확인
    (블록 4방향 회전)
    - 채운 answer return
    
    2. 블록 이동하기
    - 드론 2칸 차지
    - 4방향 이동, 가로, 세로 방향 4가지 회전
    - 먼저 도착하는 count return
    
    3. 숫자타자대회
    - dp[left][right][idx]
    - idx 누르는데 left, right 위치에서 걸리는 최단 시간
    - 왼쪽, 오른쪽에서 눌느 경우 최소 구하기
    - 누적된 return dp 구하기

---
+++ src/프로그래머스/level3/숫자타자대회10.java
@@ -0,0 +1,87 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 숫자타자대회10 {
+
+    static int[] numarr;
+    static int[][][] dp;
+    static int[][] pos = {
+            {0,0},
+            {0,1},
+            {0,2},
+            {1,0},
+            {1,1},
+            {1,2},
+            {2,0},
+            {2,1},
+            {2,2},
+            {3,1}
+    };
+
+    public static int solution(String numbers){
+        dp = new int[10][10][numbers.length()];
+        numarr = new int[numbers.length()];
+
+        for(int i=0;i<numbers.length();i++){
+            numarr[i] = numbers.charAt(i) - '0';
+        }
+
+        for(int[][] d1 : dp){
+            for(int[] d2 : d1){
+                Arrays.fill(d2, -1);
+            }
+        }
+
+        int answer = 0;
+
+        answer = dfs(0, 4, 6);
+
+        return answer;
+    }
+
+    static int dfs(int idx, int left, int right){
+        if(idx == numarr.length) return 0;
+        if(dp[left][right][idx] != -1) return dp[left][right][idx];
+
+
+        int target = numarr[idx];
+        int leftCount = Integer.MAX_VALUE;
+        int rightCount = Integer.MAX_VALUE;
+
+        if(right != target){
+            leftCount = getCount(left, target) + dfs(idx+1, target, right);
+        }
+
+        if(left != target){
+            rightCount = getCount(right, target) + dfs(idx+1, left, target);
+        }
+
+
+        return dp[left][right][idx] = Math.min(leftCount, rightCount);
+
+    }
+
+    static int getCount(int a, int b){
+        if(a == b) return 1;
+
+        int[] a1 = pos[a];
+        int[] b1 = pos[b];
+
+        int dx = Math.abs(a1[0]-b1[0]);
+        int dy = Math.abs(a1[1]-b1[1]);
+
+        int daegak = Math.min(dx,dy);
+        int straight = Math.max(dx,dy) - daegak;
+
+        return 3 * daegak + 2 * straight;
+    }
+
+    public static void main(String[] args) throws Exception{
+        String numbers = "1756";
+        System.out.println(solution(numbers));
+    }
+
+
+}
