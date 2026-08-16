6dce6bf1471a3ed369255ebe9b64d327f4f4e1db
JeongWooMyeong <jwm9010@gmail.com>
2026-08-16 15:29:51 +0900

    260816
    1. 노드사이의 거리
    - LCA
    binarylifting
    visited depth, dist, parent 2차운 저장
    lca -> u,v u 깊이 큰걸로 바꿈
    깊이 같아질떄까지
    아니면 동시에 바꿈
    - 다익스트라로 풀 수 있음
    
    2. 찾기
    - KMP 알고리즘
    - 패턴분석 -> 텍스트 검색
    
    3. 전화번호목록
    - trie (트라이)
    - 문자 한갰ㄱ
    Node child = new Node[10]
    boolean isEnd;

---
+++ src/프로그래머스/level4/찾기4.java
@@ -0,0 +1,63 @@
+package 프로그래머스.level4;
+
+import java.util.*;
+import java.io.*;
+
+public class 찾기4 {
+
+    static String text;
+    static String pattern;
+    static List<Integer> result;
+    static int L;
+    static int[] pi;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        text = br.readLine();
+        pattern = br.readLine();
+        result = new ArrayList<>();
+
+        L = pattern.length();
+        pi = new int[L];
+
+        for(int i=1,j=0;i<L;i++){
+            while(j > 0 && pattern.charAt(i) != pattern.charAt(j)){
+                j = pi[j-1];
+            }
+
+            if(pattern.charAt(i) == pattern.charAt(j)){
+                j++;
+            }
+
+            pi[i] = j;
+        }
+
+        for(int i=0,j=0;i<text.length();i++){
+            while(j > 0 && text.charAt(i) != pattern.charAt(j)){
+                j = pi[j-1];
+            }
+
+            if(text.charAt(i) == pattern.charAt(j)){
+                j++;
+            }
+
+            if(j == pattern.length()){
+                result.add(i - pattern.length() + 2);
+                j = pi[j-1];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(result.size()).append("\n");
+
+        for(int x : result){
+            sb.append(x).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+
+    }
+
+}
