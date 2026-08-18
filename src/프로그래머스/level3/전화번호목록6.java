069ea708b051178127fc30776bb62fca8009ab3d
JeongWooMyeong <jwm9010@gmail.com>
2026-08-18 20:06:49 +0900

    260818
    했던거 복습
    
    1. 파티
    - 다익스트라 연습
    
    2. 노드사이의 거리
    - lca
    
    3. 찾기
    - kmp
    
    4. 전화번호 목록
    - trie (트라이)
    
    5. 기둥과 보설치
    - 기둥과 보 설치 조건 구현

---
+++ src/프로그래머스/level3/전화번호목록6.java
@@ -0,0 +1,94 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class 전화번호목록6 {
+
+    static int t;
+    static int n;
+    static class Node{
+        Node[] child = new Node[10];
+        boolean isEnd;
+    }
+    static StringBuilder sb;
+    static String[] numbers;
+    static Node root;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        t = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+
+        while(t-- > 0 ){
+            n = Integer.parseInt(br.readLine());
+            numbers = new String[n];
+
+            root = new Node();
+
+            for(int i=0;i<n;i++){
+                numbers[i] = br.readLine();
+                insert(numbers[i]);
+            }
+
+            boolean found = true;
+
+            for(int i=0;i<n;i++){
+                if(!isConsistent(numbers[i])){
+                    found = false;
+                    break;
+                }
+            }
+
+            if(found) sb.append("YES").append("\n");
+            else sb.append("NO").append("\n");
+
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static void insert(String number){
+        Node cur = root;
+
+        for(int i=0;i<number.length();i++){
+            int idx = number.charAt(i) - '0';
+
+            if(cur.child[idx] == null){
+                cur.child[idx] = new Node();
+            }
+
+            cur = cur.child[idx];
+
+        }
+
+        cur.isEnd = true;
+
+    }
+
+    static boolean isConsistent(String number){
+        Node cur = root;
+
+        for(int i=0;i<number.length();i++){
+            int idx = number.charAt(i) - '0';
+            cur = cur.child[idx];
+
+            if(i < number.length()-1 && cur.isEnd){
+                return false;
+            }
+
+        }
+
+        for(Node child : cur.child){
+            if(child != null){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
