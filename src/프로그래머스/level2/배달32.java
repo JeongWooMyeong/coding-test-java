45a1f9ee4db48487f084719882a856d5ee90cba6
JeongWooMyeong <jwm9010@gmail.com>
2026-08-25 17:50:03 +0900

    260825
    1. 배달
    - 다익스트라, 벨만포드 연습
    
    2. 택시합승요금
    - 플로이드 워셜 알고리즘 연습
    
    3. 섬연결하기
    - MST 크루스칼
    - 프림 알고리즘 연습

---
+++ src/프로그래머스/level2/배달32.java
@@ -0,0 +1,92 @@
+package 프로그래머스.level2;
+
+import java.util.*;
+import java.io.*;
+
+public class 배달32 {
+
+    static int answer;
+    static List<List<Edge>> edges;
+    static int[] d;
+    static int INF = (int) 1e9;
+    static class Edge implements Comparable<Edge>{
+        int to, cost;
+
+        public Edge(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+
+        public int compareTo(Edge other){
+            return Integer.compare(this.cost, other.cost);
+        }
+
+    }
+
+    public static int solution(int N, int[][] road, int K){
+
+        answer = 0;
+        edges = new ArrayList<>();
+        for(int i=0;i<=N;i++){
+            edges.add(new ArrayList<>());
+        }
+
+        d = new int[N+1];
+        Arrays.fill(d, INF);
+
+        for(int[] r : road){
+            int a = r[0];
+            int b = r[1];
+            int cost = r[2];
+
+            edges.get(a).add(new Edge(b,cost));
+            edges.get(b).add(new Edge(a,cost));
+
+        }
+
+        dijkstra(1);
+
+        for(int i=1;i<=N;i++){
+            if(d[i] <= K){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    static void dijkstra(int start){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(start, 0));
+        d[start] = 0;
+
+        while(!pq.isEmpty()){
+            Edge cur = pq.poll();
+            int now = cur.to;
+            int dist = cur.cost;
+
+            if(d[now] < dist) continue;
+
+            for(int i=0;i<edges.get(now).size();i++){
+                int next = edges.get(now).get(i).to;
+                int cost = d[now] + edges.get(now).get(i).cost;
+
+                if(d[next] > cost){
+                    d[next] = cost;
+                    pq.offer(new Edge(next, cost));
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int N = 6;
+        int[][] road = {{1,2,1},{1,3,2},{2,3,2},{3,4,3},{3,5,2},{3,5,3},{5,6,1}};
+        int K = 4;
+
+        System.out.println(solution(N, road, K));
+    }
+
+
+}
