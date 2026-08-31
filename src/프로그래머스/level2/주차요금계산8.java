df37a34a9f0685e94c484beaf088ee8290ebc29c
JeongWooMyeong <jwm9010@gmail.com>
2026-08-31 20:58:35 +0900

    260831
    1. 프렌즈 4블록
    - board char 저장
    - results 2x2 되는지 확인
    - 제거한 칸 계산 후 빈칸 처리
    - 나머지 있는 블록들 아래로 내리기 (거꾸로)
    - return answer;
    
    2. 주차요금 계산
    - intime, totaltime (누적)
    - 남은 intime은 23:59 이용해서 구하기
    - cannum 순으루 정렬 후 요금 계산
    - return 배열 answer

---
+++ src/프로그래머스/level2/주차요금계산8.java
@@ -0,0 +1,80 @@
+package 프로그래머스.level2;
+
+import java.util.*;
+import java.io.*;
+
+public class 주차요금계산8 {
+
+    static Map<Integer, Integer> intime;
+    static Map<Integer, Integer> totaltime;
+    static int[] answer;
+
+    public static int[] solution(int[] fees, String[] records){
+
+        intime = new HashMap<>();
+        totaltime = new HashMap<>();
+
+        for(String r : records){
+            String[] arr = r.split(" ");
+            int sec = toMin(arr[0]);
+            int num = Integer.parseInt(arr[1]);
+            String cmd = arr[2];
+
+            if("IN".equals(cmd)){
+                intime.put(num, sec);
+            }else{
+                totaltime.put(num, totaltime.getOrDefault(num, 0) + sec - intime.get(num));
+                intime.remove(num);
+            }
+        }
+
+        for(int num : intime.keySet()){
+            totaltime.put(num, totaltime.getOrDefault(num, 0) + toMin("23:59") - intime.get(num));
+        }
+
+        List<Integer> carList = new ArrayList<>();
+        for(int num : totaltime.keySet()){
+            carList.add(num);
+        }
+
+        Collections.sort(carList);
+        answer = new int[carList.size()];
+
+        for(int i=0;i<carList.size();i++){
+            answer[i] = getFee(totaltime.get(carList.get(i)), fees);
+        }
+
+        return answer;
+    }
+
+    static int toMin(String time){
+        String[] arr = time.split(":");
+        int H = Integer.parseInt(arr[0]) * 60;
+        int M = Integer.parseInt(arr[1]);
+
+        return H + M;
+    }
+
+    static int getFee(int time, int[] fees){
+        int basictime = fees[0];
+        int basicfee = fees[1];
+        int unittime = fees[2];
+        int unitfee = fees[3];
+
+
+        if(time < basictime ){
+            return basicfee;
+        }
+
+        return basicfee + (int)Math.ceil((double)(time-basictime) / unittime) * unitfee;
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[] fees = {180, 5000, 10, 600};
+        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+
+        System.out.println(Arrays.toString(solution(fees, records)));
+    }
+
+}
