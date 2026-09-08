f2ad4c267941e53d53ef485e5abb9cea3419d56b
JeongWooMyeong <jwm9010@gmail.com>
2026-09-08 21:46:13 +0900

    260908
    1. 평범한 배낭
    - dp[j] = 배낭의 허용 무게가 j일 때 얻을 수 있는 최대 가치
    
    2. LCS
    - 최소 공통 수여
    - dp[i][j]
      = 첫 번째 문자열 i개와
        두 번째 문자열 j개까지 봤을 때
        LCS의 최대 길이
    
    3. 가장긴증가하는 부분수열
    - dp[i]를
    
      i번째 원소를 마지막 원소로 하는 가장 긴 증가 부분 수열의 길이

---
+++ src/프로그래머스/level3/LCS3.java
@@ -0,0 +1,39 @@
+package 프로그래머스.level3;
+
+import java.util.*;
+import java.io.*;
+
+public class LCS3 {
+
+    static int[][] dp;
+    static int n,m;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        char[] c1 = str1.toCharArray();
+        char[] c2 = str2.toCharArray();
+
+        n = c1.length;
+        m = c2.length;
+
+        dp = new int[n+1][m+1];
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(c1[i-1] == c2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+
+}
